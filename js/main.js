675114b992e2f96fd9a8cfca01474522841d165c
sarvarabduganiyev <sarvarabduganiyev636@gmail.com>
2022-01-23 20:15:25 +0500

    fotogalareyya full page qoshildi yana TAB qoshildi yana bazi ishlar

--- js/main.js
+++ js/main.js
@@ -79,4 +79,25 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
-// accardion js
+// accardion js TUGASHI
+
+let Primeneniya = document.querySelector("#Primeneniya");
+let Parametr = document.querySelector("#Parametr");
+Primeneniya.style.display = "none";
+Parametr.style.display = "none";
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tab__content");
+  tabcontent[1].style.display = "none";
+  tabcontent[2].style.display = "none";
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("maxsulotlar__full__a__tags");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" maxsulotlar__full__a__tags__active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " maxsulotlar__full__a__tags__active";
+}
